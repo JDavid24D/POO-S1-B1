8e96db033ba02661d9bd98c986d18a4917587ebd
Jdavid <jorgedaviddiaz62@gmail.com>
2025-04-30 11:49:58 -0500

    Ejemplos

---
+++ Proyecto01/src/proyecto01/proyecto01.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author utpl
+ */
+public class proyecto01 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 3;
+        int res = 0;
+        res = a - b;
+        
+        int contador = 1;
+        while(contador <= 100){
+            System.out.println(contador+ ". No debo llegar tarde a clases");
+            contador++;
+        }
+        System.out.println("Halo mundo te saluda papito Rene Elizalde\n" + 
+                "Resultado resta: " + res);
+    }
+}
