6134acad318670e7ec2ba93a74fc25f5cc85fa0a
Jdavid <jorgedaviddiaz62@gmail.com>
2025-04-30 12:00:26 -0500

    Ejemplos

--- Proyecto01/src/proyecto01/proyecto01.java
+++ Proyecto01/src/proyecto01/proyecto01.java
@@ -9,22 +9,24 @@ package proyecto01;
  * @author utpl
  */
 public class proyecto01 {
+
     public static void main(String[] args) {
         int a = 1;
         int b = 3;
         int res = 0;
         res = a - b;
-        
+
         int contador = 1;
 //        while(contador <= 100){
 //            System.out.println(contador+ ". No debo llegar tarde a clases");
 //            contador++;
 //        }
-        
-        do{
-            System.out.println(contador+ ". No debo llegar tarde a clases");
+
+        do {
+            System.out.println(contador + ".No debo llegar tarde a clases");
             contador++;
-        }while(contador <= 100);
+        } while (contador <= 100);
+        
         System.out.println("Halo mundo\n" + "Resultado resta: " + res);
     }
 }
