b43d478a7b077ade0c95d4e534e54b9482d6f1f2
Jdavid <jorgedaviddiaz62@gmail.com>
2025-04-30 12:15:31 -0500

    Ejemplos

--- Proyecto01/src/proyecto01/proyecto01.java
+++ Proyecto01/src/proyecto01/proyecto01.java
@@ -17,7 +17,7 @@ public class proyecto01 {
         res = a - b;
 
         int contador = 1;
-/*        while(contador <= 100){
+        /*        while(contador <= 100){
             System.out.println(contador+ ". No debo llegar tarde a clases");
             contador++;
         }
@@ -26,9 +26,13 @@ public class proyecto01 {
             System.out.println(contador + ".No debo llegar tarde a clases");
             contador++;
        } while (contador <= 100);
-*/       
-        for(int i = 100; i >=1; i= i-2){
-             System.out.println(i+ ".No debo llegar tarde a clases");
+         
+        for (int i = 1; i <= 100; i = i+2) {
+            System.out.println(i + ".No debo llegar tarde a clases");
+        }
+*/
+        for (int i = 100; i >= 1; i--) {
+            System.out.println(i + ".No debo llegar tarde a clases");
         }
         System.out.println("Halo mundo\n" + "Resultado resta: " + res);
     }
